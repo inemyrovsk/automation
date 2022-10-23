5dfc6b05b2fffe5f0353b9175dde311878e9fc40
ivan_n <ivan.n@itsyndicate.org>
2022-10-23 15:19:22 +0300

    auto install

---
+++ minikube-install.sh
@@ -0,0 +1,13 @@
+sudo apt update -y
+sudo apt install -y curl apt-transport-https wget ca-certificates gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo cp minikube-linux-amd64 /usr/local/bin/minikube
+sudo chmod +x /usr/local/bin/minikube
+sudo systemctl start docker
+minikube start --driver=docker
